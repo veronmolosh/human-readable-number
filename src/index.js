cb02e2bba1ad72a1d325e93bf01ec22592ddfb21
veronmolosh <nikabu17@gmail.com>
2021-03-13 18:06:59 +0300

    feat: add solution task human-readable-number 991/1000

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numToTwenty = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+        'twenty'
+    ];
+    const tens = [
+        '',
+        '',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety'
+    ];
+
+    let i;
+    let stringReturn;
+    let stringReturnHundred;
+    let numReadable1;
+    let numReadable2;
+
+    for (i = 21; i < 1000; i++) {
+        if (i < 100) {
+            if (i % 10 === 0) {
+                stringReturn = ''; //если нет остатка от деления, то ни чего не передаем
+            } else {
+                stringReturn = ' ' + numToTwenty[i % 10];
+            }
+            numReadable1 = tens[Math.floor(i / 10)] + stringReturn;
+            numToTwenty.push(numReadable1);
+        } else {
+            if (i % 100 === 0) {
+                stringReternHundred = ''; //если нет остатка от деления, то ни чего не передаем
+            } else {
+                stringReturnHundred = ' ' + numToTwenty[i % 100];
+            }
+            numReadable2 = numToTwenty[Math.floor(i / 100)] + ' hundred' + stringReturnHundred;
+            numToTwenty.push(numReadable2);
+        }
+    }
+    return numToTwenty[number];
 }
