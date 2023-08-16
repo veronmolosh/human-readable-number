55620a5a8d62725673c3ae05374e0e7d1da6b977
veronmolosh <nikabu17@gmail.com>
2023-08-16 16:02:35 -0400

    update solution

--- src/index.js
+++ src/index.js
@@ -1,64 +1,30 @@
 module.exports = function toReadable(number) {
-    const numToTwenty = [
-        'zero',
-        'one',
-        'two',
-        'three',
-        'four',
-        'five',
-        'six',
-        'seven',
-        'eight',
-        'nine',
-        'ten',
-        'eleven',
-        'twelve',
-        'thirteen',
-        'fourteen',
-        'fifteen',
-        'sixteen',
-        'seventeen',
-        'eighteen',
-        'nineteen',
-        'twenty'
-    ];
-    const tens = [
-        '',
-        '',
-        'twenty',
-        'thirty',
-        'forty',
-        'fifty',
-        'sixty',
-        'seventy',
-        'eighty',
-        'ninety'
-    ];
+	const words = [
+		"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+		"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+	];
 
-    let i;
-    let stringReturn;
-    let stringReturnHundred;
-    let numReadable1;
-    let numReadable2;
+	const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
-    for (i = 21; i < 1000; i++) {
-        if (i < 100) {
-            if (i % 10 === 0) {
-                stringReturn = ''; //если нет остатка от деления, то ни чего не передаем
-            } else {
-                stringReturn = ' ' + numToTwenty[i % 10];
-            }
-            numReadable1 = tens[Math.floor(i / 10)] + stringReturn;
-            numToTwenty.push(numReadable1);
-        } else {
-            if (i % 100 === 0) {
-                stringReternHundred = ''; //если нет остатка от деления, то ни чего не передаем
-            } else {
-                stringReturnHundred = ' ' + numToTwenty[i % 100];
-            }
-            numReadable2 = numToTwenty[Math.floor(i / 100)] + ' hundred' + stringReturnHundred;
-            numToTwenty.push(numReadable2);
-        }
-    }
-    return numToTwenty[number];
+	if (number === 0) {
+		return "zero";
+	}
+
+	if (number < 20) {
+		return words[number];
+	}
+
+	if (number < 100) {
+		const ten = Math.floor(number / 10);
+		const unit = number % 10;
+		return `${tens[ten]}${unit !== 0 ? " " + words[unit] : ""}`;
+	}
+
+	if (number < 1000) {
+		const hundred = Math.floor(number / 100);
+		const remainder = number % 100;
+		return `${words[hundred]} hundred${remainder !== 0 ? " " + toReadable(remainder) : ""}`;
+	}
+
+	return "Number out of range";
 }
